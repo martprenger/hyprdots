0366e320d5153cd2da3a58335b8fe0b7d77a1393
martprenger <mart.prenger@gmail.com>
2024-06-05 12:44:39 +0200

    laptop relevant changes

--- Configs/.config/hypr/scripts/getworkspace.sh
+++ Configs/.config/hypr/scripts/getworkspace.sh
@@ -14,10 +14,7 @@ active_monitor_id=$(echo "$hyprctl_output" | jq '.[] | select(.focused == true) 
 
 # Assign values based on the screen name
 case "$active_monitor_id" in
-    0) value=1 ;;
-    1) value=2 ;;
-    2) value=0 ;;
-    *) value=999 ;; # Default value for unknown screen names
+    *) value=0 ;; # Default value for unknown screen names
 esac
 
 # Perform the calculation (multiply by 10 and add to the parameter)
