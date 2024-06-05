7c316ad70827c944e05341cfb1fac13fc232b4ed
martprenger <mart.prenger@gmail.com>
2024-06-05 20:09:48 +0200

    added popup for volume mute

--- Configs/.config/ags/widget/osd/OSD.ts
+++ Configs/.config/ags/widget/osd/OSD.ts
@@ -1,43 +1,43 @@
-import { icon } from "lib/utils"
-import icons from "lib/icons"
-import Progress from "./Progress"
-import brightness from "service/brightness"
-import options from "options"
+import { icon } from "lib/utils";
+import icons from "lib/icons";
+import Progress from "./Progress";
+import brightness from "service/brightness";
+import options from "options";
 
-const audio = await Service.import("audio")
-const { progress, microphone } = options.osd
+const audio = await Service.import("audio");
+const { progress, microphone } = options.osd;
 
-const DELAY = 2500
+const DELAY = 2500;
 
-function OnScreenProgress(vertical: boolean) {
+function OnScreenProgress(vertical) {
     const indicator = Widget.Icon({
         size: 42,
         vpack: "start",
-    })
+    });
     const progress = Progress({
         vertical,
         width: vertical ? 42 : 300,
         height: vertical ? 300 : 42,
         child: indicator,
-    })
+    });
 
     const revealer = Widget.Revealer({
         transition: "slide_left",
         child: progress,
-    })
+    });
 
-    let count = 0
-    function show(value: number, icon: string) {
-        revealer.reveal_child = true
-        indicator.icon = icon
-        progress.setValue(value)
-        count++
+    let count = 0;
+    function show(value, icon) {
+        revealer.reveal_child = true;
+        indicator.icon = icon;
+        progress.setValue(value);
+        count++;
         Utils.timeout(DELAY, () => {
-            count--
+            count--;
 
             if (count === 0)
-                revealer.reveal_child = false
-        })
+                revealer.reveal_child = false;
+        });
     }
 
     return revealer
@@ -52,39 +52,68 @@ function OnScreenProgress(vertical: boolean) {
         .hook(audio.speaker, () => show(
             audio.speaker.volume,
             icon(audio.speaker.icon_name || "", icons.audio.type.speaker),
-        ), "notify::volume")
+        ), "notify::volume");
 }
 
 function MicrophoneMute() {
     const icon = Widget.Icon({
         class_name: "microphone",
-    })
+    });
 
     const revealer = Widget.Revealer({
         transition: "slide_up",
         child: icon,
-    })
+    });
 
-    let count = 0
-    let mute = audio.microphone.stream?.is_muted ?? false
+    let count = 0;
+    let mute = audio.microphone.stream?.is_muted ?? false;
 
     return revealer.hook(audio.microphone, () => Utils.idle(() => {
         if (mute !== audio.microphone.stream?.is_muted) {
-            mute = audio.microphone.stream!.is_muted
-            icon.icon = icons.audio.mic[mute ? "muted" : "high"]
-            revealer.reveal_child = true
-            count++
+            mute = audio.microphone.stream!.is_muted;
+            icon.icon = icons.audio.mic[mute ? "muted" : "high"];
+            revealer.reveal_child = true;
+            count++;
 
             Utils.timeout(DELAY, () => {
-                count--
+                count--;
                 if (count === 0)
-                    revealer.reveal_child = false
-            })
+                    revealer.reveal_child = false;
+            });
         }
-    }))
+    }));
+}0
+
+function VolumeMute() {
+    const icon = Widget.Icon({
+        class_name: "volume",
+    });
+
+    const revealer = Widget.Revealer({
+        transition: "slide_up",
+        child: icon,
+    });
+
+    let count = 0;
+    let mute = audio.speaker.stream?.is_muted ?? false;
+
+    return revealer.hook(audio.speaker, () => Utils.idle(() => {
+        if (mute !== audio.speaker.stream?.is_muted) {
+            mute = audio.speaker.stream!.is_muted;
+            icon.icon = icons.audio.volume[mute ? "muted" : "high"];
+            revealer.reveal_child = true;
+            count++;
+
+            Utils.timeout(DELAY, () => {
+                count--;
+                if (count === 0)
+                    revealer.reveal_child = false;
+            });
+        }
+    }));
 }
 
-export default (monitor: number) => Widget.Window({
+export default (monitor) => Widget.Window({
     monitor,
     name: `indicator${monitor}`,
     class_name: "indicator",
@@ -106,6 +135,11 @@ export default (monitor: number) => Widget.Window({
                 vpack: microphone.pack.v.bind(),
                 child: MicrophoneMute(),
             }),
+            Widget.Box({
+                hpack: microphone.pack.h.bind(),
+                vpack: microphone.pack.v.bind(),
+                child: VolumeMute(),
+            }),
         ),
     }),
-})
+});
