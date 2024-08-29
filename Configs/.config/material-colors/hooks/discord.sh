4c66939efef0c95c6aaa7265dd5878888717b66d
martprenger <mart.prenger@gmail.com>
2024-08-29 20:21:15 +0200

    changes that i don't remember

--- Configs/.config/material-colors/hooks/discord.sh
+++ Configs/.config/material-colors/hooks/discord.sh
@@ -2,3 +2,4 @@
 material_cache="$HOME"/.cache/material/
 
 sass $material_cache/material-discord.scss $material_cache/material-discord.css
+cp $material_cache/material-discord.css "$HOME"/.config/BetterDiscord/themes/material-discord.theme.css
---
+++ Configs/.config/material-colors/hooks/zed.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+material_cache="$HOME"/.cache/material/
+
+cp $material_cache/material-zed.json "$HOME"/.config/zed/themes/material-zed.json
